1a089765a7ce32ef4ba86829b3ff5ec170a23aca
Tiago Bizze <Tiago Bizze>
2023-09-26 18:41:42 -0300

    add controller

---
+++ app/Http/Controllers/PessEnderecoController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PessEndereco;
+use App\Http\Requests\StorePessEnderecoRequest;
+use App\Http\Requests\UpdatePessEnderecoRequest;
+
+class PessEnderecoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePessEnderecoRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(PessEndereco $pessEndereco)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(PessEndereco $pessEndereco)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatePessEnderecoRequest $request, PessEndereco $pessEndereco)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(PessEndereco $pessEndereco)
+    {
+        //
+    }
+}
